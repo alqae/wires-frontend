3b821f01261f4606f4ce4321be1f2865f225219b
Johan Martinez <js.martinez.sandino@gmail.com>
2024-02-24 14:00:00 -0500

    wip: setup message gateway (socket)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import * as fromComponents from './shared/components';
 import { HttpClientModule } from '@angular/common/http';
+
+const config: SocketIoConfig = { url: 'http://localhost:81', options: {} };
 
 @NgModule({
   declarations: [],
@@ -14,6 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
     SharedModule,
     HttpClientModule,
     AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [fromComponents.LayoutComponent]
